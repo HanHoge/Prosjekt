f5aa1fc4d430cb0cb736e75ba51e143ae0abefc5
HanHoge <38155228+HanHoge@users.noreply.github.com>
2018-08-06 21:24:12 +0200

    add all

--- MINIZED/Noko_nytt/Noko_nytt.srcs/sources_1/new/Signal.vhd
+++ MINIZED/Noko_nytt/Noko_nytt.srcs/sources_1/new/Signal.vhd
@@ -32,12 +32,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Signal is
---  Port ( );
+--generic ( teller : intiger
+Port (
+        CLK : std_logic;
+        RST : std_logic;
+        bitin   : std_logic_vector(15 downto 0);
+        bitout  : std_logic_vector(15 downto 0)
+        );
 end Signal;
 
 architecture Behavioral of Signal is
 
+type t_array_taps is array(0 to 6-1) of signed (15 downt 0) ;
+signal taps : t_arrat_taps;
+
+
 begin
 
+filter: process (clk) is
+variable sum :integer;
+begin
+    if rising_edge(clk) then
+        sum = to_integer(x * 5580));
+        sum = to_integer(sum + (taps(0) * 5750));
+        sum = to_integer(sum + (taps(1) * 6936));
+        sum = to_integer(sum + (taps(2) * 6936));
+        sum = to_integer(sum + (taps(3) * 5750));
+        sum = to_integer(sum + (taps(4) * 5580));
+        taps(0) <= x;
+        for ii in 1 to 5-1 loop
+            taps(ii) <= taps((ii - 1));
+        end loop;
+        y <= to_signed(sum, 16);
+    end if;
+end process filter;
 
 end Behavioral;
