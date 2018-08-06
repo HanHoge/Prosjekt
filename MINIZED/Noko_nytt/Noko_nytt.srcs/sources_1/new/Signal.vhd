f72844e9cca82d18e577056e8fa3556b0e4e7de6
HanHoge <38155228+HanHoge@users.noreply.github.com>
2018-08-06 18:41:33 +0200

    add all

---
+++ MINIZED/Noko_nytt/Noko_nytt.srcs/sources_1/new/Signal.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.08.2018 18:00:22
+-- Design Name: 
+-- Module Name: Signal - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Signal is
+--  Port ( );
+end Signal;
+
+architecture Behavioral of Signal is
+
+begin
+
+
+end Behavioral;
